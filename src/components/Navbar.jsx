c67b3e1b95a383740c04ac83c93607584416b126
SleimaD <sleima@icloud.com>
2026-01-08 23:10:32 +0100

    Final Implementation of 3D Product Viewer Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { navlinks } from '../constants'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
@@ -8,7 +8,7 @@ const Navbar = () => {
             <img src="/logo.svg" alt='Apple logo'/>
 
             <ul>
-                {navlinks.map(({ label }) => (
+                {navLinks.map(({ label }) => (
                     <li key={label}>
                         <a href={label}>{label}</a>
                     </li>
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ const useMacbookStore = create((set) => ({
     setColor: (color) => set({color}),
     scale: 0.08,
     setScale: (scale) => set({scale}),
-    reset: () => set({ color: '#2e2c3e', scale: 0.08}),
+    reset: () => set({ color: '#2e2c2e', scale: 0.08}),
 }))
 
 export default useMacbookStore;
